1c7666669ea2b2c6ac0691d1f60f02524bec356b
Isaac Harris <IsaacHphotography@gmail.com>
2022-08-31 18:15:46 -0600

    Updated all javascript and css

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+var questions = [ //This file was created for all questions to be asked for the quiz. They are made to be randomized in the test.
+    { 
+        question: "Which company developed JavaScript?", 
+        answers: [
+            { text: "Some dude in his garage", correct: false },
+            { text: "Netscape" , correct: true },
+            { text: "Google", correct: false },
+            { text: "Mozilla", correct: false }
+        ]
+    },
+    { 
+        question: "What are all the looping structures in JavaScript?", 
+        answers: [
+            { text: "String, For-while, Cancel", correct: false },
+            { text: "Array, Var, Rotate", correct: false },
+            { text: "For, While, Do-while loops", correct: true },
+            { text: "Head, Body, Header", correct: false }
+        ]
+    },
+    { 
+        question: "What is the use of the blur function?", 
+        answers: [
+            { text: "Blur function is used to remove the focus from the specified object.", correct: true },
+            { text: "To blur out sections of Java I no longer like.", correct: false },
+            { text: "Used to blur the webpage when loading.", correct: false },
+            { text: "All the above", correct: false }
+        ]
+    },
+    { 
+        question: "What is ‘this’ keyword in JavaScript?", 
+        answers: [
+            { text: "Used to combine var objects", correct: false },
+            { text: "‘This’ keyword refers to the object from where it was called.", correct: true },
+            { text: "Not a function used in JavaScript", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+    { 
+        question: "What is JavaScript??",
+        answers: [
+            { text: "Something most web developers stay away from", correct: false },
+            { text: "A language used for calculating my tip at a restaurant", correct: false },
+            { text: "JavaScript is a client-side and server-side scripting language inserted into HTML pages and is understood by web browsers. JavaScript is also an Object-based Programming language", correct: true },
+            { text: "None of the above", correct: false }
+            
+        ]
+    },
+];
